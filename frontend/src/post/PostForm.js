0230af7ce0c019840e3f8e4a24c774966ff6d4a5
iamthuypham <tpham045@hotmail.com>
2017-10-09 08:09:57 +0000

    feat: add new post form
    Users can add a post to a post list of selected category.
    When users navigate to different category, category field is auto-updated based on path
    
    States of all input fields are component state.
    
    `PostForm` receives:
    - `categories` props to display category selection list
    - `category` props to set initial state for `category`. This state is updated every time there is a modified props
    
    Generating new post id with `UUID` package

---
+++ frontend/src/post/PostForm.js
@@ -0,0 +1,77 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+const uuidv4 = require('uuid/v4');
+
+class PostForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = ({
+    	openForm: false,
+      	title: '',
+      	body: '',
+      	author: '',
+      	category: this.props.currentCategory
+    })
+    this.handleForm = this.handleForm.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
+  }
+  handleForm() {
+  	this.setState({openForm: !this.state.openForm})
+  }
+  handleSubmit(e) {
+    e.preventDefault();
+    const newPost = {
+      	id: uuidv4(),
+    	title: this.state.title,
+      	category: this.state.category,
+      	body: this.state.body,
+      	author: this.state.author,
+      	timestamp: Date.now(),
+    }
+    console.log(newPost)
+  }
+  componentWillReceiveProps(nextProps) {
+  	if (this.props.category !== nextProps.category) {
+    	this.setState({
+          	openForm: false,
+            title: '',
+            body: '',
+            author: '',
+        	category: nextProps.category
+        })
+    }
+  }
+  render() {
+    const { categories } = this.props.categories
+    const { title, author, body, category } = this.state
+    return (
+  	  <div>
+      	<button onClick={this.handleForm}>New Post</button>
+        {this.state.openForm &&
+          <form onSubmit={this.handleSubmit}>
+               <input type="text" name="title" placeholder="New Title" value={title} onChange={(e) => this.setState({ title: e.target.value })} required/>
+			   <label>
+                  Current category:
+                  <select value={this.state.category} onChange={(e) => this.setState({ category: e.target.value })}>
+					<option value=''>Select a category</option>
+					{categories && categories.map((category) => 
+                    	<option key={category.name} value={`${category.name}`}>{category.name}</option>
+                    )}
+                  </select>
+                </label>
+               <input type="text" name="body" placeholder="New Content" value={body} onChange={(e) => this.setState({ body: e.target.value })} required/>
+               <input type="text" name="author" placeholder="Your Name" value={author} onChange={(e) => this.setState({ author: e.target.value })} required/>
+               <input type='submit' value='Submit'/>
+          </form>
+        }
+      </div>
+	)
+  }
+}
+
+PostForm.PropTypes = { 
+	categories: PropTypes.array.isRequired
+}
+
+export default PostForm
